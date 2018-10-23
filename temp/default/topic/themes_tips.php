33be58ac688546715a7f9939a333f745f5aa8757
ElementLS <lishuai891026@163.com>
2018-10-23 11:26:15 +0800

    1、删除了themes_tips.php中的帖子显示形式的内容（只留下了图标导航）；
    2、删除了common1.35.css文件中关于“class=find-items”中可以滑动的部分的设置。

--- temp/default/topic/themes_tips.php
+++ temp/default/topic/themes_tips.php
@@ -17,28 +17,3 @@
     </div>
 
 </div>
-
-<!--{loop $list $value}-->
-<div id="theme_$value[vid]">
-  <div class="b_c3 bob o_c4 pt10">
-    <!--{if $value['video']}-->
-    <h3 class="theme-sub"><a href="topic.php?vid=$value['vid']" class="load">$value['subject']</a></h3>
-    <div class="theme-play"><a {if $_S['setting']['qiniu_play']}href="$value['video']" class="playvideo"{else}href="topic.php?vid=$value['vid']" class="load"{/if}><div class="theme-video video_hode"><img src="{$value['video']}?vframe/jpg/offset/{$_S['setting']['qiniu_frame']}" onerror="this.onerror=null;this.src='./ui/novideocover.png'" /><span class="icon icon-play"></span></div></a></div>
-    
-    <!--{else}-->
-    <!--{if $value['pics']==1}-->
-    <div class="theme-content cl">
-      <a href="topic.php?vid=$value['vid']" class="load block"><!--{eval piclist($value)}--><h3 class="theme-sub">$value['subject']</h3></a>
-    </div>
-    <!--{else}-->
-    <a href="topic.php?vid=$value['vid']" class="load"><h3 class="theme-sub">$value['subject']</h3>
-    <!--{if $value['pics']>1}-->
-    <ul class="theme-img cl" id="pics_$value[vid]">
-      <!--{eval piclist($value,200,150,3)}-->
-    </ul></a>
-    <!--{/if}-->  
-    <!--{/if}-->
-    <!--{/if}-->
-  </div>
-</div>
-<!--{/loop}-->
